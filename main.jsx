ec8f35e13a04bc725e0492e015dfd1a03cac59df
jimnuyu <jiminh428@gmail.com>
2026-09-07 22:56:22 +0900

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import GuppyMateChoiceLab3D from './GuppyMateChoiceLab3D.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GuppyMateChoiceLab3D />
+  </React.StrictMode>
+);
